efde07aa58c7a3990d0733b5c9eabe476a61f6d4
Joe Bracey <joebracey1@gmail.com>
2024-12-01 21:26:33 +0000

    Default created time to the time it was created

--- issueTrackerBackendApp/models.py
+++ issueTrackerBackendApp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Issue(models.Model):
     issue_name = models.CharField(max_length=100)
     issue_description = models.CharField(max_length=500)
     issue_status = models.BooleanField()
-    date_created = models.DateField()
+    date_created = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
         return f'{self.issue_name} was created on {self.date_created}'
